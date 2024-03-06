faca77867edd8695956622ae921d82144b84b227
“Scotlyn <st699@byu.edu>
2024-03-06 14:43:51 -0700

    ServiceTests all pass

--- server/src/test/java/serviceTests/ServiceTests.java
+++ server/src/test/java/serviceTests/ServiceTests.java
@@ -1,10 +1,15 @@
 package serviceTests;
 
 import ResponseTypes.LoginResponse;
+import chess.ChessGame;
 import dataAccess.DataAccessException;
+import dataAccess.GameDAO;
 import model.AuthData;
+import model.GameData;
 import org.junit.jupiter.api.*;
 import service.Service;
+
+import java.util.HashSet;
 
 public class ServiceTests {
 
@@ -48,7 +53,7 @@ public class ServiceTests {
 
   @Test
   @DisplayName("Login Fail")
-  public void badLogin() throws DataAccessException {
+  public void badLogin() {
     Assertions.assertThrows(DataAccessException.class, () -> {
       service.login(existingUsername, existingPassword);
     });
@@ -57,48 +62,115 @@ public class ServiceTests {
   @Test
   @DisplayName("Logout Success")
   public void goodLogout() throws DataAccessException {
-
+    AuthData authToken = service.register(existingUsername, existingPassword, existingEmail);
+    service.login(existingUsername, existingPassword);
+    service.logout(authToken.authToken());
   }
 
   @Test
   @DisplayName("Logout Fail")
   public void badLogout() throws DataAccessException {
-
+    service.register(existingUsername, existingPassword, existingEmail);
+    service.login(existingUsername, existingPassword);
+    Assertions.assertThrows(DataAccessException.class, () -> {
+      service.logout(null);
+    });
   }
 
   @Test
   @DisplayName("CreateGame Success")
   public void goodCreateGame() throws DataAccessException {
-
+    service.register(existingUsername, existingPassword, existingEmail);
+    LoginResponse login = service.login(existingUsername, existingPassword);
+    int gameID = service.addGame("myGame", login.authToken());
+    Assertions.assertEquals(1, gameID);
+    Assertions.assertEquals(1, service.listGames(login.authToken()).size());
   }
 
   @Test
   @DisplayName("CreateGame Fail")
   public void badCreateGame() throws DataAccessException {
-
+    service.register(existingUsername, existingPassword, existingEmail);
+    LoginResponse login = service.login(existingUsername, existingPassword);
+    Assertions.assertThrows(DataAccessException.class, () -> {
+      service.addGame("myGame", null);
+    });
   }
 
   @Test
   @DisplayName("ListGames Success")
   public void goodListGames() throws DataAccessException {
-
+    service.register(existingUsername, existingPassword, existingEmail);
+    LoginResponse login = service.login(existingUsername, existingPassword);
+    HashSet<GameData> expectedList = new HashSet<>();
+    service.addGame("myGame", login.authToken());
+    service.addGame("yay chess", login.authToken());
+    service.addGame("queens only", login.authToken());
+    expectedList.add(new GameData(1, null, null, "myGame", new ChessGame()));
+    expectedList.add(new GameData(2, null, null, "yay chess", new ChessGame()));
+    expectedList.add(new GameData(3, null, null, "queens only", new ChessGame()));
+    Assertions.assertEquals(expectedList, service.listGames(login.authToken()));
   }
 
   @Test
   @DisplayName("ListGames Fail")
   public void badListGames() throws DataAccessException {
-
+    service.register(existingUsername, existingPassword, existingEmail);
+    LoginResponse login = service.login(existingUsername, existingPassword);
+    HashSet<GameData> expectedList = new HashSet<>();
+    service.addGame("myGame", login.authToken());
+    service.addGame("yay chess", login.authToken());
+    service.addGame("queens only", login.authToken());
+    expectedList.add(new GameData(1, null, null, "myGame", new ChessGame()));
+    expectedList.add(new GameData(2, null, null, "yay chess", new ChessGame()));
+    expectedList.add(new GameData(3, null, null, "queens only", new ChessGame()));
+    Assertions.assertThrows(DataAccessException.class, () -> {
+      service.listGames(null);
+    });
   }
 
   @Test
   @DisplayName("JoinGame Success")
   public void goodJoinGame() throws DataAccessException {
+    HashSet<GameData> expectedList = new HashSet<>();
+
+    service.register(existingUsername, existingPassword, existingEmail);
+    service.register("newUser",  "newPassword", "new@gmail.com");
+    service.register("watchUser", "watchPass", "watch@gmail.com");
+
+    LoginResponse existingUserLogin = service.login(existingUsername, existingPassword);
+    LoginResponse newUserLogin = service.login("newUser", "newPassword");
+    LoginResponse watchUserLogin = service.login("watchUser", "watchPass");
+
+    service.addGame("myGame", existingUserLogin.authToken());
+    service.joinGame("WHITE", 1, existingUserLogin.authToken());
+    service.joinGame("BLACK", 1, newUserLogin.authToken());
+
+    expectedList.add(new GameData(1, existingUsername, "newUser", "myGame", new ChessGame()));
+    Assertions.assertEquals(expectedList, service.listGames(existingUserLogin.authToken()));
+
+    // join to watch
+    Assertions.assertDoesNotThrow(() -> {
+      service.joinGame("empty", 1, watchUserLogin.authToken());
+    });
 
   }
 
   @Test
   @DisplayName("JoinGame Fail")
   public void badJoinGame() throws DataAccessException {
+    service.register(existingUsername, existingPassword, existingEmail);
+    LoginResponse login = service.login(existingUsername, existingPassword);
+    service.addGame("myGame", login.authToken());
+    service.joinGame("WHITE", 1, login.authToken());
 
+    // try and join when taken
+    Assertions.assertThrows(DataAccessException.class, () -> {
+      service.joinGame("WHITE", 1, login.authToken());
+    });
+
+    Assertions.assertThrows(DataAccessException.class, () -> {
+      service.joinGame("BLACK", 1, null);
+    });
   }
 }
